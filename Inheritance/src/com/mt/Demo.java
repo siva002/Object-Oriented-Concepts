c9cbb3245ebbe79b5b31208f1f19b63b2a13d377
Siva Nalavenkata <siva002@gmail.com>
2012-03-09 00:09:37 +0530

    Inheritance DEMO

--- Inheritance/src/com/mt/Demo.java
+++ Inheritance/src/com/mt/Demo.java
@@ -1,15 +1,41 @@
 package com.mt;
 
 public class Demo {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println ( "First line of program" );
-		
 
+		// Instantiate a new Book Class
+		Book b = new Book();
+		b.weight = 1.0f;
+		
+		Phone p = new Phone ();
+		p.weight = 1.0f;
+		
+		laptop l = new laptop ();
+		l.weight = 1.0f;
+		
+		System.out.println ("The frieght per KG of this book is "+ b.calculate_freight() );
+		System.out.println ("The frieght per KG of this Phone is "+ p.calculate_freight() );
+		System.out.println ("The frieght per KG of this Laptop is "+ l.calculate_freight() );
+		
+		material m1 = new Book();
+		m1.weight = 1.0f;
+		
+		material m2 = new Phone ();
+		m2.weight = 1.0f;
+		
+		material m3 = new laptop ();
+		m3.weight = 1.0f;
+		
+		System.out.println (m1.weight);
+		System.out.println ("The frieght per KG of this book is "+ m1.calculate_freight() );
+		System.out.println ("The frieght per KG of this Phone is "+ m2.calculate_freight() );
+		System.out.println ("The frieght per KG of this Laptop is "+ m3.calculate_freight() );
+		
+		
 	}
+
 
 }
